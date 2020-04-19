73b116a1781fd03717d498fdaa0b3f56cc5ac6df
maabro1920daw2 <maabro1920daw2@gmail.com>
2020-04-19 20:18:08 +0200

    mapa primeras marcas

--- app/views/HomeView.js
+++ app/views/HomeView.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Dimensions, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
-//Estilos para los mapas, creados por google
-import mapStyle from '../../assets/style_map.json';
-
+import MapView, { Marker } from 'react-native-maps'; //Importar mapas
+import mapStyle from '../../assets/style_map.json'; //Estilos para los mapas, creados por google
 
 export class HomeView extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
+        this.state = { //Las coordenadas que se inician al principio
             region: {
                 latitude: 41.450390,
                 longitude: 2.247568,
@@ -25,12 +23,15 @@ export class HomeView extends React.Component {
                     region={this.state.region}
                     style={styles.sizes}
                     customMapStyle={mapStyle}
-                />
+                >
+                    <Marker coordinate={{latitude: 41.449847, longitude: 2.247616}} />
+                    <Marker coordinate={{latitude: 41.449331, longitude: 2.24709}} />
+                </MapView>
             </View>            
         );
     }
 }
-
+//Estilos HomeView
 const styles = StyleSheet.create({
     sizes: {
         width: Dimensions.get('window').width,
