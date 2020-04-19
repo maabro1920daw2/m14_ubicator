42eba3d6f86d268558d20352b300e069c05caf81
maabro1920daw2 <maabro1920daw2@gmail.com>
2020-04-19 17:53:34 +0200

    mapa y instalacion sqlite

--- app/views/HomeView.js
+++ app/views/HomeView.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { StyleSheet, Dimensions, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+//Estilos para los mapas, creados por google
 import mapStyle from '../../assets/style_map.json';
 
-//const mapStyle = require('./assets/style_map.json');
 
 export class HomeView extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            region: {
+                latitude: 41.450390,
+                longitude: 2.247568,
+                latitudeDelta: 0.0122,
+                longitudeDelta: 0.0121,
+            },
+        };
     }
+
     render(){
         return(
             <View>
-                <MapView style={styles.sizes} customMapStyle={mapStyle}/>
+                <MapView 
+                    region={this.state.region}
+                    style={styles.sizes}
+                    customMapStyle={mapStyle}
+                />
             </View>            
         );
     }
@@ -21,6 +34,6 @@ export class HomeView extends React.Component {
 const styles = StyleSheet.create({
     sizes: {
         width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        height: '100%',
     },
 });
