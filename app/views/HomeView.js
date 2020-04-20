5e6e820e01d5d3b0d49304829eddc8b680d0d9e3
maabro1920daw2 <maabro1920daw2@gmail.com>
2020-04-20 21:58:34 +0200

    markers puestos con la bd

--- app/views/HomeView.js
+++ app/views/HomeView.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { StyleSheet, Dimensions, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps'; //Importar mapas
 import mapStyle from '../../assets/style_map.json'; //Estilos para los mapas, creados por google
+import * as SQLite from 'expo-sqlite'; //Importar SQLite
+
+const db = SQLite.openDatabase("db.db"); //Llamada a la base de datos
 
 export class HomeView extends React.Component {
     constructor(props){
@@ -13,7 +16,27 @@ export class HomeView extends React.Component {
                 latitudeDelta: 0.0122,
                 longitudeDelta: 0.0121,
             },
+            markers: [], //Guarda la información de los puntos de interes
         };
+
+        db.transaction(tx => {
+            tx.executeSql("drop table if exists locals;"); //Borra la tabla
+            tx.executeSql( //Crea la tabla
+                "create table if not exists locals (id integer primary key not null, latitude real, longitude real, name text);"
+            );
+            //Inserts
+            tx.executeSql("insert into locals (latitude, longitude, name) values (41.449847, 2.247616, ?)", ['Frankfurt Badalona']);
+            tx.executeSql("insert into locals (latitude, longitude, name) values (41.449331, 2.24709, ?)", ['4 Pedres']);
+            //Select de los datos
+            tx.executeSql("select * from locals", [], (tx,results) => {
+                let temp = []; //Array temporal para guardar las filas  
+                for(let i=0; i < results.rows.length; i++){
+                    temp.push(results.rows.item(i));
+                }
+                console.log(temp);
+                this.setState({ markers: temp, }); //Pone el state markers con la info de la BD
+            });
+        });
     }
 
     render(){
@@ -24,8 +47,9 @@ export class HomeView extends React.Component {
                     style={styles.sizes}
                     customMapStyle={mapStyle}
                 >
-                    <Marker coordinate={{latitude: 41.449847, longitude: 2.247616}} />
-                    <Marker coordinate={{latitude: 41.449331, longitude: 2.24709}} />
+                    {this.state.markers.map(marker => ( //Muestra los puntos del mapa
+                        <Marker key={marker.id} coordinate={{latitude: marker.latitude, longitude: marker.longitude}} />
+                    ))}
                 </MapView>
             </View>            
         );
